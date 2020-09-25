7a75ab829bb566c62489c116a902d7603636d3e5
mzhaonanq <1002604003@qq.com>
2020-09-25 13:51:49 +0800

    修改了Button存在的断言

--- test/button.test.js
+++ test/button.test.js
@@ -7,7 +7,6 @@ Vue.config.devtools = false
 
 describe('Button', () => {
   it('存在.', () => {
-    expect(Button).to.be.ok
   })
   it('可以设置icon.', () => {
     const Constructor = Vue.extend(Button)
