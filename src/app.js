a3dea623d34f424232bc397e20d5b6789580761e
mzhaonanq <1002604003@qq.com>
2020-09-27 23:18:18 +0800

    使用 ‘scss loop’ 完成column span

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Button from  './button'
 import Input from './input'
-import Icon from  './icon'
 import ButtonGroup from './buttonGroup'
+import Row from './row'
+import Column from './column'
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-buttongroup',ButtonGroup)
 Vue.component('g-input',Input)
+Vue.component('g-row',Row)
+Vue.component('g-column',Column)
 
 new Vue ({
   el:'#app',
---
+++ src/column.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="column" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    span:{
+      type:[Number,String]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.column{
+  height: 100px;
+  background: grey;
+  width: 50%;
+  border:1px solid red;
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
+    }
+  }
+}
+</style>
