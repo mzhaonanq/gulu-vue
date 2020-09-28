afa845da4a4901d5103c2810f8f96cb47dd152f8
mzhaonanq <1002604003@qq.com>
2020-09-28 17:40:41 +0800

    初步完成响应式布局，默认为移动端

--- src/column.vue
+++ src/column.vue
@@ -5,30 +5,58 @@
     </div>
 </template>
 <script>
+let validator = (value) => {
+  let keys = Object.keys(value)
+  return keys.map(key =>['span','offset'].includes(key))
+}
 export default {
-  data(){
-    return{
+  data() {
+    return {
       gutter: 0
     }
   },
-  computed:{
-    colClass(){
-      let {span,offset} = this
-    return  [span&&`col-${span}`,offset&&`offset-${offset}`]
+  computed: {
+    colClass() {
+      let {span, offset, ipad, narrowPc, pc, wildPc} = this
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`,
+        ...(ipad ? [`col-ipad-${ipad.span}`] : []),
+        ...(narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
+        ...(pc ? [`col-pc-${pc.span}`] : []),
+        ...(wildPc ? [`col-wild-pc-${wildPc.span}`] : []),
+      ]
     },
-    colStyle(){
-       let {gutter}=this
-      return {paddingLeft: gutter/2 +'px', paddingRight: gutter/2 + 'px'}
+    colStyle() {
+      let {gutter} = this
+      return {paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px'}
+    },
+  },
+
+  props: {
+    span: {
+      type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    },
+    ipad: {
+      type: Object,
+      validator
+    },
+    narrowPc: {
+      type: Object,
+      validator
+    },
+    pc: {
+      type: Object,
+      validator
+    },
+    wildPc: {
+      type: Object,
+      validator
     }
   },
-  props:{
-    span:{
-      type:[Number,String]
-    },
-    offset:{
-      type:[Number,String]
-    }
-  }
 }
 </script>
 <style lang="scss" scoped>
@@ -44,6 +72,41 @@ export default {
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       margin-left: ($n / 24) * 100%;
+    }
+  }
+  @media(min-width: 557px) and(max-width: 768px) {
+    $class-prefix: col-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media(min-width: 769px) and(max-width: 992px) {
+    $claas-prefix: col-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media(min-width: 993px) and (max-width: 1200px) {
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+  }
+
+  @media(min-width: 1200px) {
+    $class-prefix: col-wild-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
     }
   }
 }
