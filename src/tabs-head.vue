bf5efd485d6e22422372dd4d8afa464bc07aebb4
mzhaonanq <1002604003@qq.com>
2020-09-30 22:50:47 +0800

    tabs可以切换页面了

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>
+  <div class="tabs-head">
     <slot></slot>
-    <slot name="action"></slot>
+    <div class="actions-wrapper">
+      <slot name="action"></slot>
+    </div>
+
   </div>
 </template>
 <script>
 export default {
   name: 'GuluTabsHead',
   inject:['eventBus'],
+
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$tab-height:40px;
+.tabs-head{
+  display: flex;
+  height: $tab-height;
+  justify-content: flex-start;
+  align-items: center;
+  border:1px solid red;
+  > .actions-wrapper{
+    margin-left: auto;
+  }
+}
+</style>
--- src/tabs-pane.vue
+++ src/tabs-pane.vue
@@ -1,17 +1,47 @@
 <template>
-  <div>
+  <div v-if="active" class="tabs-pane" :class="classes">
     <slot></slot>
   </div>
 </template>
 <script>
 export default {
   name: 'GuluTabsPane',
-  inject:['eventBus'],
-  created(){
-    this.eventBus.$on('update:selected',name=>{
-      console.log(name);
+  inject: ['eventBus'],
+  data() {
+    return {
+      active: false
+    }
+  },
+  props: {
+    name: {
+      type: String | Number,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  created() {
+    this.eventBus.$on('update:selected', name => {
+   this.active = name===this.name;
     })
   },
+  computed:{
+    classes(){
+      return {
+        active: this.active
+      }
+    },
+  },
+
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.tabs-pane{
+  &.active{
+    background: red;
+  }
+}
+</style>
