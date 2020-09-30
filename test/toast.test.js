1a77cced7fd644ca0f9f6ec3739c7440264059ad
mzhaonanq <1002604003@qq.com>
2020-09-30 20:27:45 +0800

    修复了toast测试用例因close事件触发太快而产生的报错

--- test/toast.test.js
+++ test/toast.test.js
@@ -24,7 +24,7 @@ describe('Toast', () => {
         done()
       })
     })
-    it('接收closeButton和callback', () => {
+    it('接收closeButton和callback', (done) => {
       const callback = sinon.fake()
       const Constructor = Vue.extend(Toast)
       const vm = new Constructor({
@@ -37,8 +37,12 @@ describe('Toast', () => {
       }).$mount()
       let closeButton = vm.$el.querySelector('.close')
       expect(closeButton.textContent.trim()).to.equal('关闭吧')
-      closeButton.click()
-      expect(callback).to.have.been.called
+      setTimeout(()=>{
+        closeButton.click()
+        expect(callback).to.have.been.called
+        done()
+      },200)
+
     })
     it('接收enableHtml',()=>{
       const Constructor = Vue.extend(Toast)
