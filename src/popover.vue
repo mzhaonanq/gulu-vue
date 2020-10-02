77fd9a19bfbf43368f8e3f5350f50a7e54417430
mzhaonanq <1002604003@qq.com>
2020-10-02 23:37:16 +0800

    表驱动编程重构positionContent

--- src/popover.vue
+++ src/popover.vue
@@ -17,9 +17,6 @@
 <script>
 export default {
   name: "GuluPopover",
-  data() {
-    return { visible: false };
-  },
   props: {
     position: {
       type: String,
@@ -29,29 +26,38 @@ export default {
       },
     },
   },
+  data() {
+    return {
+      visible: false,
+    };
+  },
   methods: {
     positionContent() {
       const { contentWrapper, triggerWrapper } = this.$refs;
       document.body.appendChild(contentWrapper);
       let { width, height, top, left } = triggerWrapper.getBoundingClientRect();
-      if (this.position === "top") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top = top + window.scrollY + "px";
-      } else if (this.position === "bottom") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top = top + height + window.scrollY + "px";
-      } else if (this.position === "left") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        let { height: height2 } = contentWrapper.getBoundingClientRect();
-        contentWrapper.style.top =
-          top + window.scrollY + (height - height2) / 2 + "px";
-      } else if (this.position === "right") {
-        contentWrapper.style.left = left + window.scrollX + width + "px";
-        let { height: height2 } = contentWrapper.getBoundingClientRect();
-        contentWrapper.style.top =
-          top + window.scrollY + (height - height2) / 2 + "px";
-      }
-    },
+      let {height:height2} = contentWrapper.getBoundingClientRect()
+      let positions = {
+        top: {
+          left: left + window.scrollX,
+          top: top + window.scrollY,
+        },
+        bottom: {
+          left: left + window.scrollX,
+          top: top + height + window.scrollY,
+        },
+        left: {
+          top: top + window.scrollY + (height - height2) / 2,
+          left: left + window.scrollX,
+        },
+          right: {
+            top: top + window.scrollY + (height - height2) / 2,
+            left: left + window.scrollX + width
+          },
+    }
+        contentWrapper.style.left = positions[this.position].left + 'px'
+        contentWrapper.style.top = positions[this.position].top + 'px'
+      },
     onClickDocument(e) {
       if (
         this.$refs.popover &&
