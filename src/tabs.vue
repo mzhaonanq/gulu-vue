61602b09dfe4e9d052cdfc3c6eef5112ce489261
mzhaonanq <1002604003@qq.com>
2020-10-02 20:14:06 +0800

    重构了tabs.vue的mounted

--- src/tabs.vue
+++ src/tabs.vue
@@ -4,50 +4,59 @@
   </div>
 </template>
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 export default {
-  name: 'GuluTabs',
+  name: "GuluTabs",
   props: {
     selected: {
       type: String,
-      required: true
+      required: true,
     },
     direction: {
       type: String,
-      default:'horizontal',
-      validator(value){
-        return ['horizontal','vertical'].indexOf(value)>=0
+      default: "horizontal",
+      validator(value) {
+        return ["horizontal", "vertical"].indexOf(value) >= 0;
+      },
+    },
+  },
+  data() {
+    return {
+      eventBus: new Vue(),
+    };
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus,
+    };
+  },
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console &&
+          console.warn &&
+          console.warn("tabs的子组件应该为tabs-head和tabs-body");
       }
-    }
-  },
-  data(){
-    return {
-      eventBus: new Vue()
-    }
-  },
-  provide(){
-    return {
-    eventBus:this.eventBus
-    }
-  },
-  created(){
-
-  },
-  mounted(){
-    if(this.$children.length===0){
-    console && console.warn && 
-    console.warn('tabs的子组件应该为tabs-head和tabs-body')
-    }
-    this.$children.forEach(vm=>{
-      if(vm.$options.name ==='GuluTabsHead'){
-        vm.$children.forEach(childVm=>{
-          if(childVm.$options.name==='GuluTabsItem'&& childVm.name===this.selected){
-          this.eventBus.$emit('update:selected',this.selected,childVm)
+    },
+    selectTab() {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === "GuluTabsHead") {
+          vm.$children.forEach((childVm) => {
+            if (
+              childVm.$options.name === "GuluTabsItem" &&
+              childVm.name === this.selected
+            ) {
+              this.eventBus.$emit("update:selected", this.selected, childVm);
+            }
+          });
         }
-        })
-      }
-    })
-  }
-}
+      });
+    },
+  },
+  mounted() {
+    this.checkChildren();
+    this.selectTab();
+  },
+};
 </script>
 <style lang="scss" scoped></style>
