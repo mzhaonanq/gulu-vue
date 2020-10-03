96b97b5111fb1a098be6e8cc08f60913a4ab8854
mzhaonanq <1002604003@qq.com>
2020-10-03 16:43:44 +0800

    添加single选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,30 +1,51 @@
 <template>
-<div class="collapseItem">
-    <div class='title' @click="open=!open">
-        {{title}}
+  <div class="collapseItem">
+    <div class="title" @click="toggle">
+      {{title}}
     </div>
-    <div class='content' v-if=open>
-    <slot></slot>
+    <div class="content" v-if="open">
+      <slot></slot>
     </div>
-</div>
+  </div>
 </template>
-
 <script>
-export default {
-  name: "GuluCollapseItem",
-  data () {
-    return {
-        open:false
-    };
-  },
-        props:{
-          title: {
-              type:String,
-              required:true
-          }
+  export default {
+    name: "GuluCollapseItem",
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
     },
-}
+    data () {
+      return {
+        open: false
+      }
+    },
+    inject: ['eventBus'],
+    mounted () {
+      this.eventBus && this.eventBus.$on('update:selected', (vm) => {
+        if (vm !== this) {
+          this.close()
+        }
+      })
+    },
+    methods: {
+      toggle () {
+        if (this.open) {
+          this.open = false
+        } else {
+          this.open = true
+          this.eventBus && this.eventBus.$emit('update:selected', this)
+        }
+      },
+      close () {
+        this.open = false
+      }
+    },
+  }
 </script>
+
 
 <style lang="scss" scoped>
 $grey:#ddd;
--- src/collapse.vue
+++ src/collapse.vue
@@ -1,25 +1,39 @@
 <template>
-<div class="collapse">
+  <div class="collapse">
     <slot></slot>
-</div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "GuluCollapse",
-  data () {
-    return {
+  import Vue from 'vue'
+  export default {
+    name: "GuluCollapse",
+    props: {
+      single: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data () {
+      return {
+        eventBus: new Vue()
+      }
+    },
+    provide () {
+      if (this.single) {
+        return {
+          eventBus: this.eventBus
+        }
+      }
     }
-  },
-
-}
+  }
 </script>
 
-<style lang="scss" scoped>
-$grey:#ddd;
-$border-radius:4px;
-.collapse{
-    border:1px solid $grey;
-    border-radius:$border-radius;
-}
+<style scoped lang="scss">
+  $grey: #ddd;
+  $border-radius: 4px;
+  .collapse {
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+  }
 </style>
