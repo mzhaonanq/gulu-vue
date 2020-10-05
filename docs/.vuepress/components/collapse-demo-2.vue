8307f785680784f22fe5e8795e951c2184a6cc1c
mzhaonanq <1002604003@qq.com>
2020-10-05 21:12:35 +0800

    完成官网collapse组件的内容

---
+++ docs/.vuepress/components/collapse-demo-2.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+      <h2>单选模式</h2>
+    <p>
+      <strong>使用预览</strong>
+    </p>
+
+    <g-collapse :selected.sync='selectedTab' single>
+        <g-collapse-item title="标题一" name='1'> 内容一</g-collapse-item>
+        <g-collapse-item title="标题二" name='2'>内容二</g-collapse-item>
+        <g-collapse-item title="标题三" name='3'>内容三</g-collapse-item>
+    </g-collapse>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+</div>
+</template>
+
+<script>
+import Collapse from '../../../src/Collapse/collapse'
+import CollapseItem from '../../../src/Collapse/collapse-item'
+export default {
+    components:{
+        'g-collapse':Collapse,
+        'g-collapse-item':CollapseItem
+    },
+  data () {
+    return {
+        selectedTab:['1'],
+        content:`
+        <g-collapse :selected.sync='selectedTab' single>
+        <g-collapse-item title="标题一" name='1'> 内容一</g-collapse-item>
+        <g-collapse-item title="标题二" name='2'>内容二</g-collapse-item>
+        <g-collapse-item title="标题三" name='3'>内容三</g-collapse-item>
+        </g-collapse>
+        `.replace(/^ {8}/gm, '').trim()
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- docs/components/collapse.md
+++ docs/components/collapse.md
@@ -3,3 +3,8 @@ title: Collapse - 折叠面板
 ---
 
 # 折叠面板
+
+<ClientOnly>
+<collapse-demo-1 />
+<collapse-demo-2 />
+</ClientOnly>
