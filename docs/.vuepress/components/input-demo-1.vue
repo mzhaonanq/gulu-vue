7dfe856537c4591deaca586b4a0891dd3956f8bf
mzhaonanq <1002604003@qq.com>
2020-10-05 21:02:51 +0800

    完成官网input组件的内容

---
+++ docs/.vuepress/components/input-demo-1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>简单用法</h2>
+    <p>
+      <strong>使用预览</strong>
+    </p>
+    <g-input></g-input>
+    <g-input value="gulugulu"></g-input>
+    <g-input value="gulugulu" disabled></g-input>
+    <g-input value="gulugulu" readonly></g-input>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<script>
+  import Input from '../../../src/input'
+  export default {
+    components: {'g-input' : Input },
+    data () {
+      return {
+        selected: '1',
+        content: `
+          <g-input></g-input>
+          <g-input value="gulugulu"></g-input>
+          <g-input value="gulugulu" disabled></g-input>
+          <g-input value="gulugulu" readonly></g-input>
+      `.replace(/^ {8}/gm, '').trim()
+      }
+    }
+  }
+</script>
---
+++ docs/.vuepress/components/input-demo-2.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="padding-top: 16px;">
+    <h2>双向绑定</h2>
+    <p>
+      <strong>预览</strong>
+    </p>
+    <g-input v-model="value"></g-input>
+    <div>
+      value: {{value}}
+    </div>
+
+    <p>
+      <strong>代码</strong>
+    </p>
+    <pre><code>{{content}}</code></pre>
+  </div>
+</template>
+<script>
+  import Input from '../../../src/input'
+  export default {
+    components: {'g-input' : Input },
+    data () {
+      return {
+        value: 'gulu',
+        content: `
+          data:{
+            value: 'gulu'
+          }
+          <g-input v-model="value"></g-input>
+          <div>
+            value: {{value}}
+          </div>
+      `.replace(/^ {8}/gm, '').trim()
+      }
+    }
+  }
+</script>
