6ae361abccad4e2c76a755d7d04e4a039fe76802
mzhaonanq <1002604003@qq.com>
2020-10-05 21:30:39 +0800

    准备发布到npm包

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-import Button from './src/button'
-import ButtonGroup from './src/buttonGroup'
-import Icon from './src/icon'
+import Button from './src/Button/button'
+import ButtonGroup from './src/Button/buttonGroup'
+import Column from './src/Grid/column'
+import Row from './src/Grid/row'
+import Icon from './src/Icon/icon'
+import Input from './src/input'
+import Content from './src/Layout/content'
+import Footer from './src/Layout/footer'
+import Header from './src/Layout/header'
+import Layout from './src/Layout/layout'
+import Sider from './src/Layout/sider'
+import Popover from './src/popover'
+import Tabs from './src/Tab/tabs'
+import TabsBody from './src/Tab/tabs-body'
+import TabsHead from './src/Tab/tabs-head'
+import TabsItem from './src/Tab/tabs-item'
+import TabsPane from './src/Tab/tabs-pane'
+import Plugin from './src/Toast/plugin'
+import Toast from './src/Toast/toast'
 
-export {Button,ButtonGroup,Icon}
+
+
+
+export {
+    Button, ButtonGroup, Icon, Input, Row, Column, Header, Layout, Sider,
+    Footer, Content, Popover, TabsItem, Tabs, TabsHead, TabsBody, TabsPane, Toast, Plugin
+}
+
