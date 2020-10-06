a6bc639ccecadeba6aedc7b6d1b5273e4a07ec5e
mzhaonanq <1002604003@qq.com>
2020-10-06 16:55:46 +0800

    优化官网Popover组件的显示

--- docs/.vuepress/components/popover-demo-1.vue
+++ docs/.vuepress/components/popover-demo-1.vue
@@ -8,25 +8,25 @@
     <g-popover>
       <g-button>上方弹出</g-button>
       <template slot="content">
-        弹出内容
+        Hello World
       </template>
     </g-popover>
     <g-popover position="bottom">
       <g-button>下方弹出</g-button>
       <template slot="content">
-        弹出内容
+        Hello World
       </template>
     </g-popover>
     <g-popover position="left">
       <g-button>左边弹出</g-button>
       <template slot="content">
-        弹出内容
+        Hello World
       </template>
     </g-popover>
     <g-popover position="right">
       <g-button>右边弹出</g-button>
       <template slot="content">
-        弹出内容
+        Hello World
       </template>
     </g-popover>
 
@@ -51,25 +51,25 @@
           <g-popover>
             <g-button>上方弹出</g-button>
             <template slot="content">
-              弹出内容
+              Hello World
             </template>
           </g-popover>
           <g-popover position="bottom">
             <g-button>下方弹出</g-button>
             <template slot="content">
-              弹出内容
+              Hello World
             </template>
           </g-popover>
           <g-popover position="left">
             <g-button>左边弹出</g-button>
             <template slot="content">
-              弹出内容
+             Hello World
             </template>
           </g-popover>
           <g-popover position="right">
             <g-button>右边弹出</g-button>
             <template slot="content">
-              弹出内容
+              Hello World
             </template>
           </g-popover>
       `.replace(/^ {8}/gm, '').trim()
@@ -77,7 +77,7 @@
     },
     methods: {
       onClickButton () {
-        this.$toast('<strong style="color:red;">加粗的提示</strong>', {
+        this.$toast('<strong style="color:red;">加粗的Hello World</strong>', {
           enableHtml: true
         })
       }
--- docs/.vuepress/components/popover-demo-2.vue
+++ docs/.vuepress/components/popover-demo-2.vue
@@ -8,25 +8,25 @@
     <g-popover trigger="hover">
       <g-button>上方弹出</g-button>
       <template slot="content">
-        弹出内容
+        Hi
       </template>
     </g-popover>
     <g-popover position="bottom" trigger="hover">
       <g-button>下方弹出</g-button>
       <template slot="content">
-        弹出内容
+        Hi
       </template>
     </g-popover>
     <g-popover position="left" trigger="hover">
       <g-button>左边弹出</g-button>
       <template slot="content">
-        弹出内容
+        Hi
       </template>
     </g-popover>
     <g-popover position="right" trigger="hover">
       <g-button>右边弹出</g-button>
       <template slot="content">
-        弹出内容
+        Hi
       </template>
     </g-popover>
 
@@ -51,25 +51,25 @@
           <g-popover trigger="hover">
             <g-button>上方弹出</g-button>
             <template slot="content">
-              弹出内容
+              Hi
             </template>
           </g-popover>
           <g-popover position="bottom" trigger="hover">
             <g-button>下方弹出</g-button>
             <template slot="content">
-              弹出内容
+              Hi
             </template>
           </g-popover>
           <g-popover position="left" trigger="hover">
             <g-button>左边弹出</g-button>
             <template slot="content">
-              弹出内容
+              Hi
             </template>
           </g-popover>
           <g-popover position="right" trigger="hover">
             <g-button>右边弹出</g-button>
             <template slot="content">
-              弹出内容
+              Hi
             </template>
           </g-popover>
       `.replace(/^ {8}/gm, '').trim()
@@ -77,7 +77,7 @@
     },
     methods: {
       onClickButton () {
-        this.$toast('<strong style="color:red;">加粗的提示</strong>', {
+        this.$toast('<strong style="color:red;">加粗Hi</strong>', {
           enableHtml: true
         })
       }
